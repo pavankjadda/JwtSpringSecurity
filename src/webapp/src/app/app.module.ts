2aeb8f3ac718d0e67210a4b993a0a53cbdd0109a
Pavan Jadda <pavankjadda@users.noreply.github.com>
2020-09-11 22:41:06 -0400

    Adds Login and Home component

--- src/webapp/src/app/app.module.ts
+++ src/webapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import {BrowserModule} from "@angular/platform-browser";
 
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
+import {HomeComponent} from "./core/home/home.component";
+import {LoginComponent} from "./core/login/login.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
