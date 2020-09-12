ae8d323653a7857379f555aadb4feeeeeea9607e
Pavan Jadda <pavankjadda@users.noreply.github.com>
2020-09-11 23:04:01 -0400

    Adds Login and Home component

--- src/webapp/src/app/app.module.ts
+++ src/webapp/src/app/app.module.ts
@@ -1,20 +1,35 @@
+import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
 import {BrowserModule} from "@angular/platform-browser";
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
 import {HomeComponent} from "./core/home/home.component";
 import {LoginComponent} from "./core/login/login.component";
+import {FooterComponent} from "./layout/footer/footer.component";
+import {HeaderComponent} from "./layout/header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
